f2f42f3e2bbb7eaf3d07486d284ea12082ddcc88
ajay-tester <ajayfelix.susaianthoniraj@tristhaglobal.com>
2024-11-22 12:09:28 +0530

    added minus method

--- etl_validation/__init__.py
+++ etl_validation/__init__.py
@@ -1,8 +1,9 @@
 from .count_validation import count_check
 from .accuracy_validation import accuracy_check
 from .duplicate_validation import duplicate_check
-from  .null_check_validation import Null_check
+from .null_check_validation import Null_check
+from .table_difference_validation import difference_check
 from .logger import log_message
 
 
-__all__ = ["count_check", "accuracy_check", "duplicate_check", "log_message","Null_check"]
+__all__ = ["count_check", "accuracy_check", "duplicate_check", "log_message","Null_check","difference_check"]
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from etl_validation.count_validation import count_check
 from etl_validation.accuracy_validation import accuracy_check
 from etl_validation.duplicate_validation import duplicate_check
 from etl_validation.null_check_validation import Null_check
+from etl_validation.table_difference_validation import difference_check
 from etl_validation.logger import log_message
 
 if __name__ == "__main__":
@@ -14,6 +15,7 @@ if __name__ == "__main__":
     accuracy_log_path = "log_files/accuracy_log.csv"
     duplicate_log_path = "log_files/duplicate_log.csv"
     not_null_log_path = "log_files/not_null_log.csv"
+    table_difference_log_path ="log_files/table_difference_log.csv"
 
     # Count Validation
     status, message = count_check(source_file_path, target_file_path, count_log_path)
@@ -28,7 +30,9 @@ if __name__ == "__main__":
     log_message(status, message)
 
     # Null Check
-
     status, message = Null_check(target_file_path,mandatory_column,not_null_log_path)
     log_message(status, message)
-    
+
+    # Table difference check source minus target and Vice Versa
+    status, message = difference_check(source_file_path,target_file_path,table_difference_log_path)
+    log_message(status, message)
